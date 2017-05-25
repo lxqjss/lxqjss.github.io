5c55c909fff502c328a6eaaf5163fc5b3ff010f0
廖进东 <lxqjss@qq.com>
2017-05-25 11:27:43 +0800

    Revert "ip精确定位与城市定位配合使用"
    This reverts commit e1d0628d33f974cc825f2dbe6a5484063df2ddfa.

--- js/mine/getIpLocation.js
+++ js/mine/getIpLocation.js
@@ -5,6 +5,20 @@ var lxqjssKey="fe7500f9bcdab914ed3d65664b287e43";//高德key
 var gaodeUrl="https://restapi.amap.com/v3/ip?ip="+ip+"&key="+lxqjssKey;
 var customExtent;
 
+$.ajax({
+	type : "get",
+	 url : gaodeUrl,//发送请求地址
+	 success:function(data){
+	 	var xmin=Number(data.rectangle.split(";")[0].split(",")[0]);
+	 	var ymin=Number(data.rectangle.split(";")[0].split(",")[1]);
+	 	var xmax=Number(data.rectangle.split(";")[1].split(",")[0]);
+	 	var ymax=Number(data.rectangle.split(";")[1].split(",")[1]);
+	  customExtent =[xmin,ymin,xmax,ymax];
+	  customExtent=ol.proj.transformExtent(customExtent, 'EPSG:4326', 'EPSG:3857');
+	  map.getView().fitExtent(customExtent,map.getSize());
+	}
+});
+
 var options={
   enableHighAccuracy:true, //是否要求高精度的地理信息，默认为false
   maximumAge:1000 //应用程序的缓存时间
@@ -19,26 +33,9 @@ function onSuccess(position){
    //返回用户位置
    var longitude =position.coords.longitude;
    var latitude = position.coords.latitude;
-   map.getView().setCenter(ol.proj.transform([longitude, latitude], 'EPSG:4326', 'EPSG:3857'));
-   map.getView().setZoom(12);
-    console.log("已经定位!");
+   map.getView().setCenter(longitude,latitude);
  }
 function onError(error){
-	//无法获取精确定位则采取ip城市定位
-	$.ajax({
-		type : "get",
-		 url : gaodeUrl,//发送请求地址
-		 success:function(data){
-		 	var xmin=Number(data.rectangle.split(";")[0].split(",")[0]);
-		 	var ymin=Number(data.rectangle.split(";")[0].split(",")[1]);
-		 	var xmax=Number(data.rectangle.split(";")[1].split(",")[0]);
-		 	var ymax=Number(data.rectangle.split(";")[1].split(",")[1]);
-		  customExtent =[xmin,ymin,xmax,ymax];
-		  customExtent=ol.proj.transformExtent(customExtent, 'EPSG:4326', 'EPSG:3857');
-		  map.getView().fitExtent(customExtent,map.getSize());
-		}
-	});
-
 	switch(error.code){
 	  case error.PERMISSION_DENIED:
 	  console.log("用户拒绝对获取地理位置的请求");
@@ -54,4 +51,3 @@ function onError(error){
 	  break;
 	}
 }
- 
