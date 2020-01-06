018b6ca1fe540773d88e9ce47fca2d6ff75d23b1
liaojd <liaojd@citydo.com.cn>
2020-01-06 18:06:18 +0800

    init mapview for changzhou

---
+++ mapview/precache-manifest.0e53eefa3d83e1b2f7b2d4cd58ad0c62.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb5ea9af861ac3fa46e5125f60e9d21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19d3641b3077cc6f4ce5",
+    "url": "/static/css/2.da2f59f5.chunk.css"
+  },
+  {
+    "revision": "09a34c6b54a2ca2e2cb2",
+    "url": "/static/css/main.d4d88880.chunk.css"
+  },
+  {
+    "revision": "19d3641b3077cc6f4ce5",
+    "url": "/static/js/2.533fbe13.chunk.js"
+  },
+  {
+    "revision": "1a26e1980298f6be243e8c05be09b50b",
+    "url": "/static/js/2.533fbe13.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09a34c6b54a2ca2e2cb2",
+    "url": "/static/js/main.c715e380.chunk.js"
+  },
+  {
+    "revision": "9f4adf3dd974f6f8d322",
+    "url": "/static/js/runtime-main.f9f3e236.js"
+  },
+  {
+    "revision": "28af1c85f967808bcfdc1426802eb3f6",
+    "url": "/static/media/Bg.28af1c85.jpg"
+  },
+  {
+    "revision": "5823506716369c5453a168f050d84c45",
+    "url": "/static/media/bg2.58235067.png"
+  },
+  {
+    "revision": "2297777e4da1bb6a1681db9fb874a9ae",
+    "url": "/static/media/composite_score.2297777e.png"
+  },
+  {
+    "revision": "5e6269de113f9732d75c8b87f180e1c6",
+    "url": "/static/media/human_bg.5e6269de.png"
+  },
+  {
+    "revision": "0dd6dfcda55e3baa1e7ce0332febb0f0",
+    "url": "/static/media/invoice1.0dd6dfcd.png"
+  },
+  {
+    "revision": "f7b19453fc58f579002320e6b8e8a19f",
+    "url": "/static/media/invoice2.f7b19453.png"
+  },
+  {
+    "revision": "259837c6ee1afd3faf3d51ec11dee3de",
+    "url": "/static/media/invoice3.259837c6.png"
+  },
+  {
+    "revision": "93f3f01075f5014a8d5ec8afdcb147fb",
+    "url": "/static/media/invoice4.93f3f010.png"
+  },
+  {
+    "revision": "74f927fbed3c41423bb649ea9324f90b",
+    "url": "/static/media/scan.74f927fb.gif"
+  }
+]);
